366f14f3cb70238f9ce2bd3f97d5b863efbbfcd1
CodeAbbas <abbax.uddin@gmail.com>
2025-10-28 13:53:53 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {                // <-- Add this server section
-    watch: {
-      usePolling: true,
-    }
-  }                        // <-- End server section
 })
